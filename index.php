2ed1d89601aed7ec43b6a244050c3576bc1d0315
Xeladaren <xeladaren@pm.me>
2020-03-22 23:56:50 +0100

    test interface

---
+++ index.php
@@ -0,0 +1,151 @@
+<!DOCTYPE HTML SYSTEM>
+
+<html xmlns="http://www.w3.org/1999/xhtml" lang="fr" xml:lang="en">
+
+	<head>
+		<meta charset="utf-8" />
+		<title>Web SMS</title>
+
+		<link rel="stylesheet" type="text/css" href="style.css" />
+	</head>
+
+	<body>
+
+      <header>
+         <a href="">link</a>
+         <a href="">link</a>
+         <a href="">link</a>
+         <a href="">link</a>
+      </header>
+
+      <nav>
+
+         <a href="">
+            <p class="number-link">+33 0 00 00 00 00</p>
+         </a>
+
+         <a href="">
+            <p class="number-link">+33 0 00 00 00 00</p>
+         </a>
+
+         <a href="">
+            <p class="number-link">+33 0 00 00 00 00</p>
+         </a>
+
+         <a href="">
+            <p class="number-link">+33 0 00 00 00 00</p>
+         </a>
+
+      </nav>
+
+      <section class="sms-conv">
+
+         <article class="sms">
+            <div class="sms-received">
+               <p class="sms-text">Test lslfdskjlflkdsf jlsd jfkldjsf lksdj lfsdjf lks <br> dsfsdfsfdsdfsdfsdggdsgsd </p>
+               <p class="sms-date">01/01/1970 00:00:00</p>
+            </div>
+         </article>
+
+         <article class="sms">
+            <div class="sms-received">
+               <p class="sms-text">Test lslfdskjlflkdsf jlsd jfkldjsf lksdj lfsdjf lks <br> dsfsdfsfdsdfsdfsdggdsgsd </p>
+               <p class="sms-date">01/01/1970 00:00:00</p>
+            </div>
+         </article>
+
+         <article class="sms">
+            <div class="sms-sended">
+               <p class="sms-text">Test lslfdskjlflkdsf jlsd jfkldjsf lksdj lfsdjf lks sfsdfsfdsdfsdfsdggdsgsd fsdfsdfs sdfsdfsdfsdf sdfsdfsdfsd fsdfdsfsdf sdfsdfsfsdsgsdgsdg sgdsgsdgsdgsdgsdg dsgsdgsdgsdg </p>
+               <p class="sms-date">01/01/1970 00:00:00</p>
+            </div>
+         </article>
+
+         <article class="sms">
+            <div class="sms-received">
+               <p class="sms-text">Test lslfdskjlflkdsf jlsd jfkldjsf lksdj lfsdjf lks <br> dsfsdfsfdsdfsdfsdggdsgsd </p>
+               <p class="sms-date">01/01/1970 00:00:00</p>
+            </div>
+         </article>
+
+         <article class="sms">
+            <div class="sms-sended">
+               <p class="sms-text">Test lslfdskjlflkdsf jlsd jfkldjsf lksdj lfsdjf lks <br> dsfsdfsfdsdfsdfsdggdsgsd </p>
+               <p class="sms-date">01/01/1970 00:00:00</p>
+            </div>
+         </article>
+
+         <article class="sms">
+            <div class="sms-received">
+               <p class="sms-text">Test lslfdskjlflkdsf jlsd jfkldjsf lksdj lfsdjf lks <br> dsfsdfsfdsdfsdfsdggdsgsd </p>
+               <p class="sms-date">01/01/1970 00:00:00</p>
+            </div>
+         </article>
+
+         <article class="sms">
+            <div class="sms-received">
+               <p class="sms-text">Test lslfdskjlflkdsf jlsd jfkldjsf lksdj lfsdjf lks <br> dsfsdfsfdsdfsdfsdggdsgsd </p>
+               <p class="sms-date">01/01/1970 00:00:00</p>
+            </div>
+         </article>
+
+         <article class="sms">
+            <div class="sms-received">
+               <p class="sms-text">Test lslfdskjlflkdsf jlsd jfkldjsf lksdj lfsdjf lks <br> dsfsdfsfdsdfsdfsdggdsgsd </p>
+               <p class="sms-date">01/01/1970 00:00:00</p>
+            </div>
+         </article>
+
+         <article class="sms">
+            <div class="sms-sended">
+               <p class="sms-text">Test lslfdskjlflkdsf jlsd jfkldjsf lksdj lfsdjf lks <br> dsfsdfsfdsdfsdfsdggdsgsd </p>
+               <p class="sms-date">01/01/1970 00:00:00</p>
+            </div>
+         </article>
+
+         <article class="sms">
+            <div class="sms-received">
+               <p class="sms-text">
+                  Test lslfdskjlflkdsf jlsd jfkldjsf lksdj lfsdjf lks dsfsdfsfdsdfsdfsdggdsgsd
+                  ffsdhdfsgdsfgdfsgdfsgdfgdfsgdsfgdfshdfshdfshdfshdhfdhdshdfshfsd
+                  fdhsdhsdfhdsfhfdshdfshdfhfdshdfhdfhdfhdfhdfshdfhdfshdfshdshfhsdhfdh
+                  hdsfhdfshdsfhfdhdfshdshfhdshfdshfdhdsfhfdshdsfdhsdhfdshdhsfhhdfs
+                  hdshdsfhdfshdshfdshfdshdfshdfshdfsdhfshfds
+               </p>
+               <p class="sms-date">01/01/1970 00:00:00</p>
+            </div>
+         </article>
+
+         <article class="sms">
+            <div class="sms-sended">
+               <p class="sms-text">Test lslfdskjlflkdsf jlsd jfkldjsf lksdj lfsdjf lks <br> dsfsdfsfdsdfsdfsdggdsgsd </p>
+               <p class="sms-date">01/01/1960 00:00:00</p>
+            </div>
+         </article>
+
+         <article class="sms">
+            <div class="sms-received">
+               <p class="sms-text">Test 😀</p>
+               <p class="sms-date">01/01/1970 00:00:00</p>
+            </div>
+         </article>
+
+         <article class="sms">
+            <div class="sms-sended">
+               <p class="sms-text">Test</p>
+               <p class="sms-date">01/01/1970 00:00:00</p>
+            </div>
+         </article>
+
+      </section>
+
+      <section class="sms-send">
+         <form class="sms-send" action="" method="post">
+            <textarea class="sms-send" name="name" rows="8" cols="80"></textarea>
+            <button class="sms-send" type="button" name="send">Send</button>
+         </form>
+      </section>
+
+   </body>
+
+</html>
